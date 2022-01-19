a4c6f4ac97edc22d5bbd2cf0c2f42579f158e24a
Alistair Vu <alistairvu@gmail.com>
2022-01-19 17:36:58 +0700

    Adjust document

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-head-element */
+/* eslint-disable react/no-invalid-html-attribute */
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <Head />
+        </head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
