16f5cdd410d80614ea1c8237ad600f482dea5b22
Alistair Vu <alistairvu@gmail.com>
2022-01-25 15:41:22 +0700

    Change collapse component

--- components/layout/NarrowHeader.tsx
+++ components/layout/NarrowHeader.tsx
@@ -11,60 +11,55 @@ import {
   DrawerOverlay,
   DrawerHeader,
   Button,
+  Box,
+  Collapse,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { HamburgerIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import SITE_MAP from '../../constants/siteMap';
 
 const NarrowHeader = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Flex
-      align="center"
-      py={2}
-      px={4}
-      shadow="md"
-      backgroundColor="whiteAlpha.900"
-    >
-      <NextLink href="/" passHref>
-        <Heading cursor="pointer">alistair&apos;s corner</Heading>
-      </NextLink>
-      <Spacer />
+    <Box shadow="md" backgroundColor="whiteAlpha.900">
+      <Flex align="center" py={2} px={4}>
+        <NextLink href="/" passHref>
+          <Heading cursor="pointer">alistair&apos;s corner</Heading>
+        </NextLink>
+        <Spacer />
 
-      <IconButton
-        icon={<HamburgerIcon h={6} w={6} />}
-        aria-label="menu"
-        variant="ghost"
-        backgroundColor="whiteAlpha.100"
-        onClick={onOpen}
-      />
+        <IconButton
+          icon={
+            isOpen ? <CloseIcon h={4} w={4} /> : <HamburgerIcon h={6} w={6} />
+          }
+          aria-label="menu"
+          variant="ghost"
+          backgroundColor="whiteAlpha.100"
+          onClick={() => {
+            isOpen ? onClose() : onOpen();
+          }}
+        />
+      </Flex>
 
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerCloseButton />
-          </DrawerHeader>
-
-          <DrawerBody>
-            {SITE_MAP.map((site) => (
-              <NextLink href={site.link} key={site.link} passHref>
-                <Button
-                  variant="ghost"
-                  w="100%"
-                  fontSize="xl"
-                  my={2}
-                  onClick={onClose}
-                >
-                  {site.title}
-                </Button>
-              </NextLink>
-            ))}
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Flex>
+      <Collapse in={isOpen}>
+        <Flex direction="column" w="100%" justify="center" align="center">
+          {SITE_MAP.map((site) => (
+            <NextLink href={site.link} key={site.link} passHref>
+              <Button
+                variant="ghost"
+                w="60%"
+                fontSize="xl"
+                my={2}
+                onClick={onClose}
+              >
+                {site.title}
+              </Button>
+            </NextLink>
+          ))}
+        </Flex>
+      </Collapse>
+    </Box>
   );
 };
 
