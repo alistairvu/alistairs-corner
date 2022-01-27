a99769f264097fe471b1146081317206ab72e9e8
Alistair Vu <alistairvu@gmail.com>
2022-01-27 18:44:25 +0700

    Add overlay

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,21 +1,36 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useDisclosure } from '@chakra-ui/react';
 import NarrowHeader from './NarrowHeader';
 import WideHeader from './WideHeader';
 import navStyles from '../../styles/header.module.css';
 
-const Layout: React.FC = ({ children }) => (
-  <>
-    <nav className={navStyles.nav}>
-      <Box display={{ base: 'none', md: 'block' }}>
-        <WideHeader />
-      </Box>
-      <Box display={{ base: 'block', md: 'none' }}>
-        <NarrowHeader />
-      </Box>
-    </nav>
+const Layout: React.FC = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    <main style={{ paddingTop: '3em', minHeight: '95vh' }}>{children}</main>
-  </>
-);
+  return (
+    <>
+      <nav className={navStyles.nav}>
+        <Box display={{ base: 'none', md: 'block' }}>
+          <WideHeader />
+        </Box>
+        <Box display={{ base: 'block', md: 'none' }}>
+          <NarrowHeader isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+          <Box
+            position="absolute"
+            t={0}
+            l={0}
+            h="100vh"
+            w="100vw"
+            backgroundColor="blackAlpha.500"
+            zIndex="100"
+            display={isOpen ? 'block' : 'none'}
+            onClick={onClose}
+          />
+        </Box>
+      </nav>
+
+      <main style={{ paddingTop: '3em', minHeight: '95vh' }}>{children}</main>
+    </>
+  );
+};
 
 export default Layout;
--- components/layout/NarrowHeader.tsx
+++ components/layout/NarrowHeader.tsx
@@ -3,7 +3,6 @@ import {
   Spacer,
   Heading,
   IconButton,
-  useDisclosure,
   Button,
   Collapse,
   SlideFade,
@@ -12,70 +11,73 @@ import NextLink from 'next/link';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import SITE_MAP from '../../constants/siteMap';
 
-const NarrowHeader = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  return (
-    <Flex
-      backgroundColor="white"
-      direction="column"
-      shadow="md"
-      overflowY="hidden"
-    >
-      <Flex align="center" py={2} px={4}>
-        <NextLink href="/" passHref>
-          <Heading cursor="pointer" onClick={onClose}>
-            alistair&apos;s corner
-          </Heading>
-        </NextLink>
-        <Spacer />
-
-        <IconButton
-          icon={
-            isOpen ? <CloseIcon h={4} w={4} /> : <HamburgerIcon h={6} w={6} />
-          }
-          aria-label="menu"
-          variant="ghost"
-          backgroundColor="whiteAlpha.100"
-          onClick={() => {
-            if (isOpen) {
-              onClose();
-            } else {
-              onOpen();
-            }
-          }}
-        />
-      </Flex>
-
-      <Collapse in={isOpen} animateOpacity>
-        <Flex
-          direction="column"
-          flex="1"
-          w="100%"
-          h="60vh"
-          justify="center"
-          align="center"
-        >
-          {SITE_MAP.map((site) => (
-            <SlideFade key={site.link} in={isOpen}>
-              <NextLink href={site.link} key={site.link} passHref>
-                <Button
-                  variant="ghost"
-                  w="100%"
-                  p={5}
-                  fontSize="4xl"
-                  my={5}
-                  onClick={onClose}
-                >
-                  {site.title}
-                </Button>
-              </NextLink>
-            </SlideFade>
-          ))}
-        </Flex>
-      </Collapse>
-    </Flex>
-  );
+type NarrowHeaderProps = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 };
+
+const NarrowHeader = ({ isOpen, onOpen, onClose }: NarrowHeaderProps) => (
+  <Flex
+    backgroundColor="white"
+    direction="column"
+    shadow="md"
+    overflowY="hidden"
+  >
+    <Flex align="center" py={2} px={4}>
+      <NextLink href="/" passHref>
+        <Heading cursor="pointer" onClick={onClose}>
+          alistair&apos;s corner
+        </Heading>
+      </NextLink>
+      <Spacer />
+
+      <IconButton
+        icon={
+          isOpen ? <CloseIcon h={4} w={4} /> : <HamburgerIcon h={6} w={6} />
+        }
+        aria-label="menu"
+        variant="ghost"
+        backgroundColor="whiteAlpha.100"
+        onClick={() => {
+          if (isOpen) {
+            onClose();
+          } else {
+            onOpen();
+          }
+        }}
+      />
+    </Flex>
+
+    <Collapse in={isOpen} animateOpacity>
+      <Flex
+        direction="column"
+        flex="1"
+        w="100%"
+        h="60vh"
+        justify="center"
+        align="center"
+        zIndex="800"
+      >
+        {SITE_MAP.map((site) => (
+          <SlideFade key={site.link} in={isOpen}>
+            <NextLink href={site.link} key={site.link} passHref>
+              <Button
+                variant="ghost"
+                w="100%"
+                p={5}
+                fontSize="4xl"
+                my={5}
+                onClick={onClose}
+              >
+                {site.title}
+              </Button>
+            </NextLink>
+          </SlideFade>
+        ))}
+      </Flex>
+    </Collapse>
+  </Flex>
+);
 
 export default NarrowHeader;
