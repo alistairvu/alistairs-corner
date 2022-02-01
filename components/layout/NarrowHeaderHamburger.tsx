8ac8ed685a64b142370e403ee1ecb0f6defcd91f
Alistair Vu <alistairvu@gmail.com>
2022-02-01 19:25:02 +0700

    Add rounded hamburger

--- components/layout/NarrowHeaderHamburger.tsx
+++
@@ -1,6 +0,0 @@
-import { chakra } from '@chakra-ui/react';
-import { Turn as Hamburger } from 'hamburger-react';
-
-const NarrowHeaderHamburger = chakra(Hamburger);
-
-export default NarrowHeaderHamburger;
